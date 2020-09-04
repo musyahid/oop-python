40785739bda9d3fdc2872e4b869e3530729e9911
Musyahid Abror <you@example.com>
2020-09-04 08:20:36 +0800

    update auth

--- 01-CRUD.py
+++ 01-CRUD.py
@@ -1,16 +1,24 @@
 class Employee :
+    mydict = {}
 
     def __init__(self, fullname, address, salary, phone) :
-        self.fullname = fullname
+        self._fullname = fullname
         self._address = address
         self._salary = salary
         self._phone = phone
 
     @staticmethod
-    def create(data):
-        data = [
-            s
-        ]
+    def create(self, data):
+        mydict = {
+            self._fullname : data['fullname']
+            self._address : data['address']
+            self._salary : data['salary']
+            self._phone : data['phone']
+        }
+
+        return mydict
+    
+    
 
 employee = Employee()
 employee.create({"fullname":"ratna putri", "address":"jakarta", "salary":5000000, "phone":"099903"})
--- 03-Auth.py
+++ 03-Auth.py
@@ -1,10 +1,30 @@
 class Auth:
+
+    def __init__(self, username, password):
+        self.__username = username
+        self.__password = password
+
     @staticmethod
     def login(data):
         if data['username'] == "root" and data['password'] == "secret":
-            print('oke')
+            self.__username = data['username']
+            self.__password = data['username']
+            print('login successfully')
         else : 
-            print("username atau passsword salah")
+            print("username atau password salah")
+    
+    def validate(data) :
+        if data['username'] == '' :
+            return false
+        elif data['password'] == '':
+            return false
+        else:
+            return true 
+
+    def user(self) :
+        return f"username : {self.__username} password : {self.__password}" 
 
 auth = Auth()
 auth.login({"username": 'root', "password": 'secret'})
+auth.validate({'username': 'root', 'password': 'secret'}) 
+auth.user()
